369a50e65a64f9cd793455d6cb62c93bbf102a34
Mohamed Hamdy <moh04591@gmail.com>
2026-01-11 09:09:26 +0200

    Auto-fill city name from map click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,20 +11,18 @@ import {
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 import Button from "./Button";
 
 function Map() {
   const { cities } = useCities();
   const [mapPostion, setMapPostion] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
-
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {
