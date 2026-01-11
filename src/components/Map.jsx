d8c66f013b5d14de50898bbb87146ae001f3060a
Mohamed Hamdy <moh04591@gmail.com>
2026-01-11 07:59:56 +0200

    Add Geolocation component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,11 +10,19 @@ import {
 } from "react-leaflet";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 function Map() {
   const { cities } = useCities();
   const [mapPostion, setMapPostion] = useState([40, 0]);
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -24,8 +32,19 @@ function Map() {
     },
     [mapLat, mapLng]
   );
+
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPostion([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
   return (
     <div className={styles.mapContainer}>
+      <Button type="position" onClick={getPosition}>
+        {isLoadingPosition ? "Loading..." : "Use your position"}
+      </Button>
       <MapContainer
         center={mapPostion}
         zoom={10}
---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export function useGeolocation(defaultPosition = null) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState(defaultPosition);
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Geolocation is not supported by your browser");
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (err) => {
+        setError(err.message);
+        setIsLoading(false);
+      }
+    );
+  }
+  return { getPosition, position, isLoading, error };
+}
